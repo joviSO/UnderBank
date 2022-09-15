efcf94aefd700cd93720f872f7b2355e140e7c40
João Victor Sousa Oliveira <joao.oliveira@maida.health>
2022-09-15 17:29:46 -0300

    criando methodo de deposito e saque

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,14 +4,17 @@ class Transaction < ApplicationRecord
   after_save :do_transaction_on_balance
 
   def do_transaction_on_balance
+    transaction_kind = self.transaction_kind
+    transaction_value = self.transaction_value
+    account_id = self.account_id
 
-    if @transaction_kind == "depositar"
-      account = Account.find(@account_id)
-      new_balance = account.balance + @transaction_value
+    if transaction_kind == "depositar"
+      account = Account.find(account_id)
+      new_balance = account.balance + transaction_value
       account.update(balance: new_balance)
-    elsif @transaction_kind == "sacar"
-      account = Account.find(@account_id)
-      new_balance = account.balance - @transaction_value
+    elsif transaction_kind == "sacar"
+      account = Account.find(account_id)
+      new_balance = account.balance - transaction_value
       account.update(balance: new_balance)
     end
   end
